2b9623ca02b33092a9e19a1f7296f4b7f1ca36bb
Evboys <evanboy@hotmail.fr>
2025-10-16 21:17:40 +0200

    Tableau sans modif et données en direct

--- app/pages/dashboard/page.tsx
+++ app/pages/dashboard/page.tsx
@@ -1,25 +1,14 @@
-import RealEstateBestProjects from '@/components/pages/real-estate/BestProjects';
-import RealEstateBestProjectsSecond from '@/components/pages/real-estate/BestProjectsSecond';
-import RealEstateCTA from '@/components/pages/real-estate/CTA';
+
 import RealEstateFooter from '@/components/pages/real-estate/Footer';
-import RealEstateHero from '@/components/pages/real-estate/Hero';
-import RealEstateListings from '@/components/pages/real-estate/Listings';
-import RealEstatePartner from '@/components/pages/real-estate/Partner';
-import RealEstateTestimonials from '@/components/pages/real-estate/Testimonials';
-import RealEstateWorkflow from '@/components/pages/real-estate/Workflow';
+import AboutHero from '@/components/second-pages/about/Hero';
+import Compare from '@/components/second-pages/pricing/Compare';
 import React from 'react';
 
 const RealEstatePage = () => {
     return (
         <section>
-            <RealEstateHero />
-            <RealEstatePartner />
-            <RealEstateListings />
-            <RealEstateBestProjects />
-            <RealEstateWorkflow />
-            <RealEstateTestimonials />
-            <RealEstateCTA />
-            <RealEstateBestProjectsSecond />
+            <AboutHero />
+            <Compare />
             <RealEstateFooter />
         </section>
     );
