8bcfd3d6c09945b4d433292ebd4f647fa83f606b
Thomas <thomas.zabalo@gmail.com>
2025-10-17 16:09:57 +0200

    remove unnecessary folder component

--- app/pages/dashboard/page.tsx
+++ app/pages/dashboard/page.tsx
@@ -1,6 +1,6 @@
 import Footer from '@/components/Footer';
-import AboutHero from '@/components/second-pages/about/Hero';
-import Compare from '@/components/second-pages/pricing/Compare';
+import AboutHero from '@/components/pages/dashboard/Hero';
+import Compare from '@/components/pages/dashboard/Compare';
 
 import React from 'react';
 
